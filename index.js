351ef3ce95d12f2592097d238839d9fef7139a47
Thomas Ellwood <taellwood@gmail.com>
2024-06-26 15:05:37 +0100

    used Omit to correctly add new Pizza without id

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var menu = [
-    { name: "Margherita", price: 8 },
-    { name: "Pepperoni", price: 10 },
-    { name: "Hawaiian", price: 10 },
-    { name: "Veggie", price: 9 },
+    { id: 1, name: "Margherita", price: 8 },
+    { id: 2, name: "Pepperoni", price: 10 },
+    { id: 3, name: "Hawaiian", price: 10 },
+    { id: 4, name: "Veggie", price: 9 },
 ];
 var cashInRegister = 100;
 var nextOrderId = 1;
 var orderQueue = [];
+function reStock() {
+    cashInRegister = cashInRegister - 30;
+}
 function addNewPizza(pizzaObj) {
     menu.push(pizzaObj);
 }
@@ -31,11 +34,23 @@ function completeOrder(orderId) {
     cashInRegister -= order.pizza.price;
     return order;
 }
-addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 });
-addNewPizza({ name: "BBQ Chicken", cost: 12 });
-addNewPizza({ name: "Spicy Sausage", cost: 11 });
-placeOrder("Chicken Bacon Ranch");
+function displayMenu(array) {
+    var container = document.getElementById('menuContainer');
+    var htmlString = '<ul>';
+    array.forEach(function (item) {
+        htmlString += "<li>".concat(item.name, "</li>");
+    });
+    htmlString += '</ul>';
+    container.innerHTML = htmlString;
+}
+displayMenu(menu);
+console.log("Menu at start", menu);
+console.log("Initial cash in register", cashInRegister);
+addNewPizza({ id: 5, name: 'Brocolli', price: 10 });
+console.log("Menu after adding Brocolli", menu);
+placeOrder("Pepperoni");
+console.log("New cash in register after order", cashInRegister);
+reStock();
+console.log("Cash in register after restock", cashInRegister);
 completeOrder(1);
-console.log("Menu:", menu);
-console.log("Cash in register:", cashInRegister);
-console.log("Order queue:", orderQueue);
+console.log("Cash in register at end", cashInRegister);
--- index.ts
+++ index.ts
@@ -1,26 +1,46 @@
-const menu = [
-  { name: "Margherita", price: 8 },
-  { name: "Pepperoni", price: 10 },
-  { name: "Hawaiian", price: 10 },
-  { name: "Veggie", price: 9 },
-]
-
-let cashInRegister = 100
-let nextOrderId = 1
+type Pizza = {
+  id: number
+  name: string
+  price: number
+}
 
 type Order = {
-  id: number,
-  pizza: { name: string, price: number },
+  id: number
+  pizza: Pizza
   status: "ordered" | "completed"
 }
 
-let orderQueue: Order[] = [];
+let cashInRegister = 100
+let nextOrderId = 1
+let nextPizzaId = 1
 
-function addNewPizza(pizzaObj) {
-  menu.push(pizzaObj)
+const menu: Pizza[] = [
+  { id: nextPizzaId++, name: "Margherita", price: 8 },
+  { id: nextPizzaId++, name: "Pepperoni", price: 10 },
+  { id: nextPizzaId++, name: "Hawaiian", price: 10 },
+  { id: nextPizzaId++, name: "Veggie", price: 9 },
+]
+
+const orderQueue: Order[] = []
+
+function addNewPizza(pizzaObj: Omit<Pizza, "id">): Pizza {
+  const newPizza: Pizza = {
+      id: nextPizzaId++,
+      ...pizzaObj
+  }
+  menu.push(newPizza)
+  return newPizza
 }
 
-function placeOrder(pizzaName) {
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 12 })
+addNewPizza({ name: "Spicy Sausage", price: 11 })
+
+
+
+
+
+function placeOrder(pizzaName: string): Order | undefined {
   const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
   if (!selectedPizza) {
       console.error(`${pizzaName} does not exist in the menu`)
@@ -32,24 +52,33 @@ function placeOrder(pizzaName) {
   return newOrder
 }
 
-function completeOrder(orderId: number) {
+function completeOrder(orderId: number): Order | undefined {
   const order = orderQueue.find(order => order.id === orderId)
   if (!order) {
-    console.error(`Order ${orderId} does not exist`)
-    return
+      console.error(`${orderId} was not found in the orderQueue`)
+      return
   }
   order.status = "completed"
-  cashInRegister -= order.pizza.price
   return order
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
-addNewPizza({ name: "BBQ Chicken", cost: 12 })
-addNewPizza({ name: "Spicy Sausage", cost: 11 })
+export function getPizzaDetail(identifier: string | number): Pizza | undefined {
+  if (typeof identifier === "string") {
+      return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase())
+  } else if (typeof identifier === "number") {
+      return menu.find(pizza => pizza.id === identifier)
+  } else {
+      throw new TypeError("Parameter `identifier` must be either a string or a number")
+  }
+}
 
-placeOrder("Chicken Bacon Ranch")
-completeOrder(1)
+
+// placeOrder("Chicken Bacon Ranch")
+// placeOrder("Pepperoni")
+// completeOrder(1)
+// placeOrder("Veggie")
+// completeOrder(2)
 
 console.log("Menu:", menu)
-console.log("Cash in register:", cashInRegister)
-console.log("Order queue:", orderQueue)
+// console.log("Cash in register:", cashInRegister)
+// console.log("Order queue:", orderQueue)
